aef04639320fad04735184f3d2e7b4609e7fd0fc
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-06 14:07:09 -0500

    project initiated

---
+++ src/main/java/edu/uptc/swi/sprintdev/SprintdevApplication.java
@@ -0,0 +1,13 @@
+package edu.uptc.swi.sprintdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uptc/swi/sprintdev/SprintdevApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uptc.swi.sprintdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
